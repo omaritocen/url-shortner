9a5b5941a1661b3c0d9e62d4f7138c9e60633254
omaritocen <omaralaaabdelkader@gmail.com>
2023-06-27 17:26:33 +0300

    Refactor code into files

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -10,56 +9,11 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-
-	r.POST("/shorten", func(c *gin.Context) {
-
-		type RequestBody struct {
-			LongUrl string `json:"longUrl" binding:"required"`
-		}
-		var body RequestBody
-
-		if err := c.ShouldBindJSON(&body); err != nil {
-			// TODO: Handle Meaningful error message
-			c.AbortWithError(http.StatusBadRequest, err)
-			return
-		}
-
-		shortUrl := getShortURLFromLongURL(body.LongUrl)
-
-		c.JSON(http.StatusCreated, gin.H{
-			"message":  "Tiny url is created",
-			"longUrl":  body.LongUrl,
-			"shortUrl": shortUrl,
-		})
-	})
-
-	r.GET("/tiny/:shortUrl", func(c *gin.Context) {
-
-		shortUrl := c.Param("shortUrl")
-
-		longUrl := GetLongURLFromShortURL(shortUrl)
-
-		c.Redirect(http.StatusMovedPermanently, longUrl)
-	})
+	r.POST("/shorten", shortenUrl)
+	r.GET("/tiny/:shortUrl", getUrl)
 
 	err := r.Run()
 	if err != nil {
 		os.Exit(-1)
 	}
-}
-
-func getShortURLFromLongURL(longUrl string) string {
-	// TODO: Implement
-	shortUrl := "x3871hkda"
-	return shortUrl
-}
-
-func GetLongURLFromShortURL(shortUrl string) string {
-	// TODO: Implement this Function
-	return "https://www.google.com"
 }
