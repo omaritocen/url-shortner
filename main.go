da4f317843da3392c32d35b88d5e4a11d952ca11
omaritocen <omaralaaabdelkader@gmail.com>
2023-06-27 17:08:27 +0300

    Create basic functions, and skeleton for logic

--- main.go
+++ main.go
@@ -16,8 +16,50 @@ func main() {
 		})
 	})
 
+	r.POST("/shorten", func(c *gin.Context) {
+
+		type RequestBody struct {
+			LongUrl string `json:"longUrl" binding:"required"`
+		}
+		var body RequestBody
+
+		if err := c.ShouldBindJSON(&body); err != nil {
+			// TODO: Handle Meaningful error message
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+
+		shortUrl := getShortURLFromLongURL(body.LongUrl)
+
+		c.JSON(http.StatusCreated, gin.H{
+			"message":  "Tiny url is created",
+			"longUrl":  body.LongUrl,
+			"shortUrl": shortUrl,
+		})
+	})
+
+	r.GET("/tiny/:shortUrl", func(c *gin.Context) {
+
+		shortUrl := c.Param("shortUrl")
+
+		longUrl := GetLongURLFromShortURL(shortUrl)
+
+		c.Redirect(http.StatusMovedPermanently, longUrl)
+	})
+
 	err := r.Run()
 	if err != nil {
 		os.Exit(-1)
 	}
+}
+
+func getShortURLFromLongURL(longUrl string) string {
+	// TODO: Implement
+	shortUrl := "x3871hkda"
+	return shortUrl
+}
+
+func GetLongURLFromShortURL(shortUrl string) string {
+	// TODO: Implement this Function
+	return "https://www.google.com"
 }
